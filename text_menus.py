39c24a62147d2fb4ec0033b383c036b1c370dbff
Hans Lo <hansshulo@gmail.com>
2013-08-21 15:39:07 -0400

    Add missing text prompts for client

---
+++ text_menus.py
@@ -0,0 +1,14 @@
+command_menu = """
+               +--------------------+
+               |      Commands      |
+               +--------------------+
+               |       move         |
+               |       interact     |
+               +--------------------+
+               """
+
+move_help    = "Move Command: Move a given character to a given location"
+interact_help = "Interact Command: Not Implemented"
+
+command_help = { "move" : move_help,
+                 "interact" : interact_help }
